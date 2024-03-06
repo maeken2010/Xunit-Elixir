7c29e698fac3409dd3ebd0e0a483cdd79111b81d
maeken2010 <maeken2010@gmail.com>
2024-03-06 22:17:32 +0900

    リファクタした
    Elixirはクラスが無いから親クラス作ってWasRunを分離する方法がわからず…

--- lib/xunit.ex
+++ lib/xunit.ex
@@ -1,26 +1,41 @@
-defmodule Xunit do
-  @moduledoc """
-  Documentation for `Xunit`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Xunit.hello()
-      :world
-
-  """
+defmodule TestCaseTest do
   def main() do
-    test = WasRun.new("test_method")
-    if test.was_run do
-      raise "error"
-    end
+    alias Xunit.WasRun
+
+    test = WasRun.new("Xunit.WasRun.test_method")
+    if test.was_run, do: raise "error"
+
     test = WasRun.run(test)
-    unless test.was_run do
-      raise "error"
-    end
+    unless test.was_run, do: raise "error"
+
+    IO.puts("ok")
+    :ok
+  end
+end
+
+defmodule Xunit.WasRun do
+  defstruct name: "", was_run: false
+
+  def new(method_name) do
+    struct!(Xunit.WasRun, %{ name: method_name }) 
+  end
+
+  def run(test) do
+    method_name = test.name
+    { result, _ } = method_name |> Code.eval_string
+    result
+  end
+
+  def test_method() do
+    struct!(Xunit.WasRun, %{ was_run: true })
+  end
+end
+
+defmodule Mix.Tasks.XunitTest do
+  use Mix.Task
+
+  def run(_) do
+    TestCaseTest.main()
   end
 end
 
